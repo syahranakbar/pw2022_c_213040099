16db1085ce40c8598c9388d7cb0bcc924774dbcf
syahranakbar <syahranakbar293@gmail.com>
2022-03-10 18:06:15 +0700

    menyelesaikan latihan pertemuan 2

---
+++ kuliah/pertemuan 2/latihan1.php
@@ -0,0 +1,49 @@
+<?php
+// KOMENTAR
+// ini untuk menuliskan catatan
+
+// VARIABEL
+// tempat penyimpanan nilai
+// Nilai: angka (integer), tulisan (string),boolean (true / false)
+// nama bebas, asal jangan pake spasi
+// boleh ada angka tapi di awal
+$nama1 ="syahranakbar";
+echo $nama1;
+echo "<hr>"
+
+// OPERATOR
+// Aritmatika
+// +, -, *, /
+echo (1 + 2) * 3 - 4; // kukabataku
+
+echo 10 "br";
+echo 10 % 5;
+echo "hr"
+
+// perbandingan
+// <, >, <=, >=, ==, !=;
+echo 1 < 2 ; // <true/1
+echo <"br"> ;
+echo 10 > 5 ; // false/ null
+echo <"br">
+
+// IDENTITAS
+// ===, !==
+// mengecek nilai beserta tipe datanya
+echo 10 === "10"
+echo <"hr">
+
+// increment/ decrement
+// penambahan/ pengurangan 1
+//++, --
+$X =10;
+echo $X++;
+echo <"br">;
+echo $X;
+echo ++$X;
+echo <"hr";
+// concat, penggabung string
+// .
+$nama_depan = "syahran"
+$nama_belakang . " "
+
